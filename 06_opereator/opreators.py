bfcb19ce826e03d05c9745bc4a75dbcc9337788a
patilyash8113 <yashpatil8113@gmail.com>
2025-12-31 11:42:19 +0530

    Add files via upload

---
+++ 06_opereator/opreators.py
@@ -0,0 +1,62 @@
+# a=50
+# b=5
+# #arithmetic operator
+# print("a+b=",a+b)
+# print("a-b=",a-b)
+# print("a*b=",a*b)
+# print("a/b=",a/b)
+# print("a%b=",a%b)
+# print("a//b=",a//b)
+# print("a ** b =",a**b)
+
+# #comparison operator
+# print(a<34)
+# print(a>34)
+# print(a<=4)
+# print(a>=50)
+# print(a==50) #is a equal to 50?
+# print(a==30) #is a equal to 30?
+# print(a!=50) #is a not equal to 50 ?
+# print(a!=30) #is a not equal to 30?
+
+c=True
+d=False
+# #logical operators
+# print(c and c) #true and true is alsways true
+# print(c and d) 
+# print(d and c )#even there is one false the answer is always a false
+#or operator
+# print(c or c)
+# print(c or d)
+# print(d or c)
+# print(d or d)
+
+# print
+
+#assignment operators
+a=10
+b=10
+c=10
+
+print(a)
+a-=10
+b+=10
+c*=10
+print(b)
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3
